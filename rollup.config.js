180de6f493375f2d0c5ac18264d4c026d867b8b6
AugustoConti <augustoconti10@gmail.com>
2020-12-02 19:11:35 -0300

    change rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,21 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import externalDeps from 'rollup-plugin-peer-deps-external';
 import typescript from 'rollup-plugin-typescript2';
-import packageJson from './package.json';
-
-const extensions = ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx'];
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: packageJson.main,
+      file: './build/index.js',
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: packageJson.module,
+      file: './build/index.es.js',
       format: 'esm',
       sourcemap: true,
     },
   ],
   external: ['react', 'react-dom'],
-  plugins: [resolve({ extensions }), commonjs(), externalDeps(), typescript()],
+  plugins: [resolve(), commonjs(), externalDeps(), typescript()],
 };
