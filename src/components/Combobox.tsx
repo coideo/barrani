58c33357cdd6742e74120fcf2acd2903648fc3e8
AugustoConti <augustoconti10@gmail.com>
2020-12-15 11:57:10 -0300

    added combobox icon

--- src/components/Combobox.tsx
+++ src/components/Combobox.tsx
@@ -20,12 +20,20 @@ import { SelectorIcon } from './icons';
 
 type Value = string | { id: string | number; name: string };
 
-const Item: FC<{ value: Value }> = ({ children, value }) => (
+const Item: FC<{ className?: string; icon?: ReactNode; value: Value }> = ({
+  children,
+  className,
+  icon,
+  value,
+}) => (
   <ComboboxOption
-    className="relative px-3 py-2 text-gray-900 cursor-pointer select-none"
+    className={cn('relative px-3 py-2 text-gray-900 cursor-pointer select-none', className)}
     value={JSON.stringify(value)}
   >
-    <span className="block font-normal truncate">{children}</span>
+    <div className="flex items-center space-x-3">
+      {icon}
+      <span className="block font-normal truncate item-text">{children}</span>
+    </div>
   </ComboboxOption>
 );
 
