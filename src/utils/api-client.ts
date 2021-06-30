f3c5713a3a65bc2646014a9b1740433b8f348a35
Matias dos Santos <matias@getcrowder.com>
2021-06-30 14:04:11 -0300

    Fix for server side requests

--- src/utils/api-client.ts
+++ src/utils/api-client.ts
@@ -26,7 +26,7 @@ function doFetch<T>(
     ...customConfig,
   };
 
-  const url = new URL(endpoint, window.location.origin);
+  const url = new URL(endpoint, inClient() ? window.location.origin : undefined);
   url.search = new URLSearchParams(queryString).toString();
 
   const promise = fetch(url.toString(), config).then(async (r) => {
